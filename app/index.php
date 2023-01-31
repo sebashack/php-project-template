cbf470201cbd2f98d82818abe0aaab1f510ed7c3
sebashack <sebastian0092@gmail.com>
2023-01-30 20:57:44 -0500

    WIP class example

--- app/index.php
+++ app/index.php
@@ -1,17 +1,27 @@
 <?php
-
-define("TANT", 50);
-
-$greeting1 = "Welcome again";
-$greeting2 = "Hello again";
-$longGreeting =<<<_STR_
-This is a long string about many things
-I want to say because it is long.
-_STR_;
-
-if (3 == 2 + 1) {
-    print(TANT);
-    print("<p>$longGreeting</p>");
-} else {
-    print("<p>$greeting2</p>");
+function genWelcome($name) {
+    return "Welcome ". $name. "<br/ >";
 }
+
+if ($_POST["name"]) {
+    if (preg_match("/[^A-Za-z'-]/", $_POST['name'])) {
+        die("Name should be alpha");
+    }
+
+    $greeting = genWelcome($_POST["name"]);
+
+    echo $greeting;
+
+    exit();
+}
+?>
+
+
+<html>
+  <body>
+    <form action = "<?php $_PHP_SELF ?>" method = "POST">
+      Name: <input type = "text" name = "name" />
+      <input type = "submit" />
+    </form>
+  </body>
+</html>
